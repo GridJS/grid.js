975e3539f22f342ede4637146dabf16afc867cf3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-05-14 01:52:05 +0600

    +1 test case for grid

--- test/grid.spec.js
+++ test/grid.spec.js
@@ -4,6 +4,7 @@
 
 var should = require('should'),
     sinon = require('sinon'),
+    _ = require('lodash'),
     utils = require('./utils.js');
 
 describe('grid', function () {
@@ -25,46 +26,20 @@ describe('grid', function () {
         });
     });
 
-    // it('should connect to at most 3 incoming clients', function (done) {
-    //     var Grid = require('../lib/grid.js');
+    it('should be able to take 32 clients', function (done) {
+        var Grid = require('../lib/grid.js');
 
-    //     var sl = require('../lib/signal-local.js')();
+        var grids = _.range(32).map(function () {
+            return new Grid({ host: 'localhost', port: 31337 });
+        });
 
-    //     var grids = [0, 1, 2, 3, 4].map(function (i) {
-    //         return new Grid({ signaling: sl });
-    //     });
+        var stb = sinon.stub();
+        stb.returns(function () { });
+        utils.when(grids, 'ready', function () {
+            _.forEach(grids, function (grid) { grid.close(); });
+            done();
+        });
 
-    //     var stb = sinon.stub();
-    //     stb.returns(function () { });
-    //     utils.when(grids, 'ready', function (id0, id1, id2, id3) {
-    //         stb.onCall(2).returns(function () {
-    //             Object.keys(grids[0].connections).should.eql([ id1, id2, id3 ]);
-    //             done();
-    //         });
-    //     });
-
-    //     grids[3].on('connection', function () { stb()(); });
-    // });
-
-        // it.only('should recover from neighbourhood connections failure', function (done) {
-        //     var Grid = require('../lib/grid.js');
-        //     var grids = [0, 1, 2, 3, 4].map(function () {
-        //         return new Grid({ host: 'localhost', port: '31337' });
-        //     });
-
-        //     var stb = sinon.stub();
-        //     stb.returns(function () { });
-
-        //     utils.when(grids, 'ready', function (id0) {
-        //         stb.onCall(2).returns(function(id) {
-        //             id.should.eql(id0);
-        //             done();
-        //         });
-
-        //         grids[1].close();
-        //         grids[2].close();
-        //         grids[3].close();
-        //         grids[3].on('connection', function(conn, id) { stb()(id); });
-        //     });
-        // });
+        grids[3].on('connection', function () { stb()(); });
+    });
 });
